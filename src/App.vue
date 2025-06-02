50087d0000976a7a31cd93727df92bc1356f1855
bharatwalejain <aarjavkjain2@gmail.com>
2025-06-02 08:36:40 +0530

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="app">
+    <ColorButton
+      v-for="(color, index) in colors"
+      :key="index"
+      :color="color"
+      @change-color="setBackgroundColor"
+    >
+      Change to {{ color }}
+    </ColorButton>
+  </div>
+</template>
+
+<script>
+import ColorButton from './components/ColorButton.vue';
+export default {
+  name: 'App',
+  components: {
+    ColorButton,
+  },
+  data() {
+    return {
+      colors: ['Black', 'Red', 'Blue', 'Green', 'Purple', 'Orange', 'White'],
+    };
+  },
+  methods: {
+    setBackgroundColor(color) {
+      document.body.style.backgroundColor = color;
+    },
+  },
+};
+</script>
+
+<style>
+.app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+}
+</style>
---
+++ src/components/ColorButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    :style="{ backgroundColor: '#000', color: '#fff', padding: '1em 2em', borderRadius: '8px', cursor: 'pointer', margin: '0.5em' }"
+    @click="handleClick"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ColorButton',
+  props: {
+    color: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    handleClick() {
+      this.$emit('change-color', this.color);
+    },
+  },
+};
+</script>
